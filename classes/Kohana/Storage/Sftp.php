dbe1fa773382f91cb203337e6533f34521adadf8
Jörn Velten <joernvigb@aol.com>
2024-11-21 13:19:36 +0100

    feat(storage-flysystem): modified visibility from private to public by visiblity-converter

--- classes/Kohana/Storage/Sftp.php
+++ classes/Kohana/Storage/Sftp.php
@@ -2,6 +2,8 @@
 
 use League\Flysystem\PhpseclibV3\SftpAdapter;
 use League\Flysystem\PhpseclibV3\SftpConnectionProvider;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
+use League\Flysystem\Visibility;
 
 /**
  * Kohana Storage
@@ -11,29 +13,30 @@ use League\Flysystem\PhpseclibV3\SftpConnectionProvider;
 class Kohana_Storage_Sftp extends Storage_Base
 {
 
-    /**
-     * Construct
-     * @param unknown $config
-     */
-    public function __construct($config)
-    {
-        $connection_provider = new SftpConnectionProvider(
-            Arr::get($config, 'host'),
-            Arr::get($config, 'username'),
+	/**
+	 * Construct
+	 * @param array $config
+	 */
+	public function __construct(array $config)
+	{
+		$connection_provider = new SftpConnectionProvider(
+			Arr::get($config, 'host'),
+			Arr::get($config, 'username'),
 
-            // from here optional
-            Arr::get($config, 'password') // password (optional, default: null) set to null if privateKey is used
-            //Arr::get($config, 'private_key'),
-            //Arr::get($config, 'pass_phrase'),
-            //Arr::get($config, 'port', 22),
-            //Arr::get($config, 'use_agent'),
-            //Arr::get($config, 'timeout'),
-            //Arr::get($config, 'max_tries'),
-            //Arr::get($config, 'finterprint_string'),
-            //Arr::get($config, 'connectivity_checker')
-        );
-        $this->_adapter = new SftpAdapter($connection_provider, Arr::get($config, 'root_path', '.'));
-    }
+			// from here optional
+			Arr::get($config, 'password') // password (optional, default: null) set to null if privateKey is used
+		//Arr::get($config, 'private_key'),
+		//Arr::get($config, 'pass_phrase'),
+		//Arr::get($config, 'port', 22),
+		//Arr::get($config, 'use_agent'),
+		//Arr::get($config, 'timeout'),
+		//Arr::get($config, 'max_tries'),
+		//Arr::get($config, 'finterprint_string'),
+		//Arr::get($config, 'connectivity_checker')
+		);
 
-    
+		$visibilityConverter = new PortableVisibilityConverter(0644, 0600, 0755, 0700, Visibility::PUBLIC);
+
+		$this->_adapter = new SftpAdapter($connection_provider, Arr::get($config, 'directory', '.'), $visibilityConverter);
+	}
 }
