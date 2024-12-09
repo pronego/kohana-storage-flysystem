d66fe16b449f481a6ccb52a2abbfbb07b2d09dd6
Jörn Velten <joernvigb@aol.com>
2024-12-09 08:43:16 +0100

    feat: refactorings

--- classes/Filesystem.php
+++ classes/Filesystem.php
@@ -3,12 +3,13 @@ use \League\Flysystem\Filesystem as FlysystemFilesystem;
 
 /**
  * Extension to support prepending and appending strings to files.
- * File is created, if it doesn't exist.
+ * File is created if it doesn't exist.
  *
  * @copyright (c) 2022 wissen.online
  * @author    Dr. Manuel Lamotte-Schubert, pronego (https://www.pronego.com)
  * @date      2022-08-31 11:18
  */
+
 class Filesystem extends FlysystemFilesystem
 {
 
@@ -25,37 +26,38 @@ class Filesystem extends FlysystemFilesystem
 	 * @param string $data
 	 * @param string $separator
 	 *
-	 * @return bool
+	 * @return void
 	 * @throws \League\Flysystem\FilesystemException
 	 */
     public function prepend(string $path, string $data, string $separator = PHP_EOL)
     {
         if ($this->fileExists($path))
         {
-            return $this->write($path, $data.$separator.$this->read($path));
+            $this->write($path, $data.$separator.$this->read($path));
         }
 
-        return $this->write($path, $data);
+        $this->write($path, $data);
     }
 
 
-    /**
-     * Append to a file.
-     *
-     * @param  string  $path
-     * @param  string  $data
-     * @param  string  $separator
-     *
-     * @return bool
-     */
-    public function append($path, $data, $separator = PHP_EOL)
+	/**
+	 * Append to a file.
+	 *
+	 * @param string $path
+	 * @param string $data
+	 * @param string $separator
+	 *
+	 * @return void
+	 * @throws \League\Flysystem\FilesystemException
+	 */
+    public function append(string $path, string $data, string $separator = PHP_EOL)
     {
         if ($this->fileExists($path))
         {
-            return $this->write($path, $this->read($path).$separator.$data);
+            $this->write($path, $this->read($path).$separator.$data);
         }
 
-        return $this->write($path, $data);
+        $this->write($path, $data);
     }
 
 
